8209061fa35124234a79f7d5a3eacea69947fc5a
Owen Medeiros <omedeiro@mit.edu>
2021-09-26 00:43:40 -0400

    Update eval_L.m

--- eval_L.m
+++ eval_L.m
@@ -1,14 +1,16 @@
-function [L] = eval_L(x, a, p, u)
+function [L] = eval_L(a, h)
 %EVAL_L equation 25
 %   p = [h]
 %   x = [x_1 x_2 x_3...]
-N = length(x)
-L = zeros(N, 1);
+N = length(a);
+L = sym(zeros(5));
 for i = 2:N-1
-    L(i) = 1/p(1)^2 * (a(i-1)*x(i-1) - 2*x(i) + a(i+1)*x(i+1)); 
+    L(i, i-1:i+1) = [conj(a(i-1)), sym(-2), a(i+1)]; 
 end
 % Boundary conditions for x, y (z is different and needs to be periodic). Supercurrent across boundry == 0 (eq 35)
-L(1) = L(2)*a(1);
-L(N) = L(N-1)*conj(a(N-1));
+% L(1,:) = L(2)*a(1).*ones(N,1)
+% L(N) = L(N-1)*conj(a(N-1));
+
+L = L./(h^2);
 end
 
