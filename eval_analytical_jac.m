761850d855dfd2ff841380b213a6a414113a0458
Owen Medeiros <omedeiro@mit.edu>
2021-09-28 09:53:55 -0400

    updates OM

--- eval_analytical_jac.m
+++ eval_analytical_jac.m
@@ -25,10 +25,10 @@ function Janalytic = eval_analytical_jac(x0, hx, N)
        if i > 1 && i < N
           Jpsidpsi(i, i-1) = exp(1i*phix(i-1))/hx^2;
           Jpsidpsi(i, i+1) = exp(-1i*phix(i))/hx^2;
-       else
-          %BCs
-          Jpsidpsi(i, i_m1) = exp(1i*phix(i_m1))/hx^2;
-          Jpsidpsi(i, i_p1) = exp(-1i*phix(i))/hx^2;
+%        else
+%           %BCs
+%           Jpsidpsi(i, i_m1) = exp(1i*phix(i_m1))/hx^2;
+%           Jpsidpsi(i, i_p1) = exp(-1i*phix(i))/hx^2;
        end
        Jpsidpsi(i, i) = -2/hx^2 +1 -2*psi(i)*conj(psi(i));
        %Jpsidpsi(i, i) = -2/hx^2 + psi(i)^2 + 2*psi(i)*conj(psi(i));
@@ -39,10 +39,10 @@ function Janalytic = eval_analytical_jac(x0, hx, N)
        if i > 1 && i < N
           Jpsidphix(i, i-1) = 1i*exp(1i*phix(i-1))*psi(i-1)/hx^2;
           Jpsidphix(i, i+1) = -1i*exp(-1i*phix(i))*psi(i+1)/hx^2;
-       else
-          %BCs
-          Jpsidphix(i, i_m1) = 1i*exp(1i*phix(i_m1))*psi(i_m1)/hx^2;
-          Jpsidphix(i, i_p1) = -1i*exp(-1i*phix(i))*psi(i_p1)/hx^2;
+%        else
+%           %BCs
+%           Jpsidphix(i, i_m1) = 1i*exp(1i*phix(i_m1))*psi(i_m1)/hx^2;
+%           Jpsidphix(i, i_p1) = -1i*exp(-1i*phix(i))*psi(i_p1)/hx^2;
        end
     end
 
@@ -58,10 +58,10 @@ function Janalytic = eval_analytical_jac(x0, hx, N)
         if i ~= N
             Jphixdpsi(i, i) = imag(exp(1i*phix(i))*psi(i+1)); 
             Jphixdpsi(i, i+1) = imag(exp(1i*phix(i)*conj(psi(i)))); 
-        else
-            % BCs
-            Jphixdpsi(i, i) = imag(exp(1i*phix(i))*psi(i_p1)); 
-            Jphixdpsi(i, i_p1) = imag(exp(1i*phix(i)*conj(psi(i)))); 
+%         else
+%             % BCs
+%             Jphixdpsi(i, i) = imag(exp(1i*phix(i))*psi(i_p1)); 
+%             Jphixdpsi(i, i_p1) = imag(exp(1i*phix(i)*conj(psi(i)))); 
         end
     end
 
@@ -69,9 +69,9 @@ function Janalytic = eval_analytical_jac(x0, hx, N)
     for i = 1:N
         if i ~= N
             Jphixdphix(i, i) = real(-exp(-1i*phix(i))*conj(psi(i))*psi(i+1)); 
-        else
-            % BCs
-            Jphixdphix(i, i) = real(-exp(-1i*phix(i))*conj(psi(i))*psi(i_p1));
+%         else
+%             % BCs
+%             Jphixdphix(i, i) = real(-exp(-1i*phix(i))*conj(psi(i))*psi(i_p1));
         end
     end
 
