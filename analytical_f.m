9fb9f934ba8a78e9fac63999cb5bfca103d8168c
Owen Medeiros <omedeiro@mit.edu>
2021-09-28 09:49:05 -0400

    Update analytical_f.m

--- analytical_f.m
+++ analytical_f.m
@@ -1,45 +1,26 @@
 % x is state variable fxn x = [psi1, psi2, ...psiN, phix1, phix2 ... phixN]
 % --> phix1 = x(N+1)
+% 
+% x(i) = x(i+1)*exp(-1i*x(N+i)); %psi1
+% x(N) = x(i-1)*exp(1i*x(N+i-1)); %psiN
 
 function F = analytical_f(x, hx, N)
     dPsidt = zeros(N, 1);
     dPhidt = zeros(N, 1);
 
-%     for i = 1:N
-%         if i==1
-% %             dPsidt(i) = -(1-x(i)*conj(x(i)))*x(i);
-%             BCPsi = x(i+1)*exp(-1i*x(N+i)); %psi0
-% %             dPsidt(i) = (1-BCPsi*conj(BCPsi))*BCPsi;
-%             dPsidt(i) = (exp(-1i*x(N+i))*x(i+1))/hx^2 + (1-BCPsi*conj(BCPsi))*BCPsi;
-% 
-%             dPhidt(i) = imag(exp(-1i*x(N+i-1))*conj(BCPsi)*x(i+1));
-%         end
-%         if i > 1 && i < N
-%             dPsidt(i) = (exp(1i*x(N + i-1))*x(i-1) - 2*x(i) + exp(-1i*x(N+i))*x(i+1))/hx^2 + (1-x(i)*conj(x(i)))*x(i);
-%             dPhidt(i) = imag(exp(-1i*x(N+i))*conj(x(i))*x(i+1));
-%         end
-%         if i == N
-% %             dPsidt(i) = -(1-x(i)*conj(x(i)))*x(i);
-%             BCPsi = x(i-1)*exp(1i*x(N+i-1)); %psiN
-% %             dPsidt(i) = (1-BCPsi*conj(BCPsi))*BCPsi;
-%             dPsidt(i) = (-2*BCPsi + exp(1i*x(N + i-1))*x(i-1) - 2*BCPsi)/hx^2 + (1-BCPsi*conj(BCPsi))*BCPsi;
-% 
-% %             dPhidt(i) = imag(exp(-1i*x(N+i-1))*conj(x(i))*x(i+1));
-%         end
-%     end
     for i = 1:N
+        if i==1
+            dPsidt(i) = (-x(i) + exp(-1i*x(N+i))*x(i+1))/hx^2 + (1-x(i)*conj(x(i)))*x(i);
+            dPhidt(i) = imag(exp(-1i*x(N+i))*conj(x(i))*x(i+1));
+        end
         if i > 1 && i < N
             dPsidt(i) = (exp(1i*x(N + i-1))*x(i-1) - 2*x(i) + exp(-1i*x(N+i))*x(i+1))/hx^2 + (1-x(i)*conj(x(i)))*x(i);
             dPhidt(i) = imag(exp(-1i*x(N+i))*conj(x(i))*x(i+1));
-        else
-            %BCs
-            i_m1 = N-1;
-            i_p1 = 2;
-            dPsidt(i) = (exp(1i*x(N + i_m1))*x(i_m1) - 2*x(i) + exp(-1i*x(N+i))*x(i_p1))/hx^2 + (1-x(i)*conj(x(i)))*x(i);
-            dPhidt(i) = imag(exp(-1i*x(N+i))*conj(x(i))*x(i_p1));
+        end
+        if i == N
+            dPsidt(i) = (exp(1i*x(N + i-1))*x(i-1) - x(i))/hx^2 + (1-x(i)*conj(x(i)))*x(i);
         end
     end
-    
     
     F = [dPsidt;dPhidt];
 end
