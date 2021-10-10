94fedc93be5694ad6a180452f4c281fcea027982
mcasteMIT <mcaste@mit.edu>
2021-10-10 17:00:41 -0400

    Update cube2column.m

--- cube2column.m
+++ cube2column.m
@@ -1,10 +1,13 @@
 function ucol = cube2column(u)
-ucol = zeros(Nx*Ny*Nz,1);
-for k = 1:Nz
-    for j = 1:Ny
-        for i = 1:Nx
-            m = Nx*(j-1)+Nx*Ny*(k-1);
-            ucol(i+m) = u(i,j,k);
+    Nx = size(u, 1);
+    Ny = size(u, 2);
+    Nz = size(u, 3);
+    ucol = zeros(Nx*Ny*Nz,1);
+    for k = 1:Nz
+        for j = 1:Ny
+            for i = 1:Nx
+                m = Nx*(j-1)+Nx*Ny*(k-1);
+                ucol(i+m) = u(i,j,k);
+            end
         end
-    end
 end
