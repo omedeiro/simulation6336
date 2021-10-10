fd890d322fc610e1fc2e5901443c27d065ea4bc7
mcasteMIT <mcaste@mit.edu>
2021-10-10 17:00:49 -0400

    Update analytical_f_xyz.m

--- analytical_f_xyz.m
+++ analytical_f_xyz.m
@@ -4,7 +4,7 @@
 % x(i) = x(i+1)*exp(-1i*x(N+i)); %psi1
 % x(N) = x(i-1)*exp(1i*x(N+i-1)); %psiN
 
-function [dPsidt; dPhidtX; dPhidtY; dPhidtZ] = analytical_f_xyz(x_int, y1_int, y2_int, y3_int, Bx, hx, hy, hz, kappa, Nx, Ny, Nz)
+function [dPsidt dPhidtX dPhidtY dPhidtZ] = analytical_f_xyz(x_int, y1_int, y2_int, y3_int, Bx, hx, hy, hz, kappa, Nx, Ny, Nz)
     %%%%%%%% _int has size (Nx-1, Ny-1, Nz-1)
     x = zeros(Nx+1, Ny+1, Nz+1);
     y1 = zeros(Nx+1, Ny+1, Nz+1);
@@ -32,15 +32,15 @@ function [dPsidt; dPhidtX; dPhidtY; dPhidtZ] = analytical_f_xyz(x_int, y1_int, y
     y1(:,:,Nz+1) = y1(:,:,2); %34
     
 
-    x(1,:,:) = x(2,:,:)*exp(-1i*y1(1,:,:)); %35
-    x(Nx+1,:,:) = x(Nx,:,:)*exp(1i*y1(Nx,:,:)); %35    
+    x(1,:,:) = x(2,:,:).*exp(-1i*y1(1,:,:)); %35
+    x(Nx+1,:,:) = x(Nx,:,:).*exp(1i*y1(Nx,:,:)); %35    
     
-    x(:,1,:) = x(:,2,:)*exp(-1i*y2(:,1,:)); %35
-    x(:,Ny+1,:) = x(:,Ny,:)*exp(1i*y2(:,Ny,:)); %35
+    x(:,1,:) = x(:,2,:).*exp(-1i*y2(:,1,:)); %35
+    x(:,Ny+1,:) = x(:,Ny,:).*exp(1i*y2(:,Ny,:)); %35
     
     % BCs on yz for Bx (36) 
-    row_B = [1 : Bx*hy*hz : Bx*hy*hz*Ny]; 
-    y3_b = row_B'*ones(1,Nz-1);
+    row_B = [1 : Bx*hy*hz : Bx*hy*hz*(Ny+1)]; 
+    y3_b = row_B'*ones(1,Nz+1);
 
     %%%% phi_z in first layer yz
     % first face
@@ -70,13 +70,18 @@ function [dPsidt; dPhidtX; dPhidtY; dPhidtZ] = analytical_f_xyz(x_int, y1_int, y
     LPHIY = construct_LPHIX(hx, hy, hz, kappa, Nx, Ny, Nz);
     LPHIZ = construct_LPHIX(hx, hy, hz, kappa, Nx, Ny, Nz);
     
-    F_PSI = construct_FPSI(x);
+    FPSI = construct_FPSI(x, Nx, Ny, Nz);
     FPHIX = construct_FPHIX(x, y1, y2, y3, hy, hz, kappa, Nx, Ny, Nz);
     FPHIY = construct_FPHIY(x, y1, y2, y3, hy, hz, kappa, Nx, Ny, Nz);
     FPHIZ = construct_FPHIZ(x, y1, y2, y3, hy, hz, kappa, Nx, Ny, Nz);
     
+    % create column vectors
+    u_x = cube2column(x(2:Nx, 2:Ny, 2:Nz));
+    u_y1 = cube2column(y1(2:Nx, 2:Ny, 2:Nz));
+    u_y2 = cube2column(y2(2:Nx, 2:Ny, 2:Nz));
+    u_y3 = cube2column(y3(2:Nx, 2:Ny, 2:Nz));
     
-    dPsidt = D*(LPSIX + LPSIY + LPSIZ)*u_x + F_PSI;
+    dPsidt = D*(LPSIX + LPSIY + LPSIZ)*u_x + FPSI;
     dPhidtX = D*(LPSIX + LPSIY + LPSIZ)*u_y1 + FPHIX;
     dPhidtY = D*(LPSIX + LPSIY + LPSIZ)*u_y2 + FPHIY;
     dPhidtX = D*(LPSIX + LPSIY + LPSIZ)*u_y3 + FPHIZ;
