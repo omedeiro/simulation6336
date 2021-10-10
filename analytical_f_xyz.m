959bef3a88c762fff7fa9d3f32f62d5b49f8c077
mcasteMIT <mcaste@mit.edu>
2021-10-10 12:17:19 -0400

    FPSI

--- analytical_f_xyz.m
+++ analytical_f_xyz.m
@@ -4,33 +4,44 @@
 % x(i) = x(i+1)*exp(-1i*x(N+i)); %psi1
 % x(N) = x(i-1)*exp(1i*x(N+i-1)); %psiN
 
-function F = analytical_f_xyz(x, y1, y2, y3, hx, hy, hz, kappa, N)
-    dPsidt = zeros(N, N, N);
-    dPhidtX = zeros(N, N, N);
-    dPhidtY = zeros(N, N, N);
-    dPhidtZ = zeros(N, N, N);
+function F = analytical_f_xyz(x, y1, y2, y3, Bx, hx, hy, hz, kappa, Nx, Ny, Nz)
+    dPsidt = zeros(Nx, Ny, Nz);
+    dPhidtX = zeros(Nx, Ny, Nz);
+    dPhidtY = zeros(Nx, Ny, Nz);
+    dPhidtZ = zeros(Nx, Ny, Nz);
     
+    % BCs on Bx
+    row_B = [1 : Bx*hy*hz : Bx*hy*hz*Ny];
+    y3_b = row_B'*ones(1,Nz-1);
 
-    %%% BOUNDARY CONDITIONS %%%
-    for k = 1:N
-        for j = 1:N
-            for i = 1:N
-                x(i,j,1) = x(i,j,N-1); %34
-                y1(i,j,1) = y1(i,j,N-1); %34
-                x(i,j,2) = x(i,j,N); %34
-                y1(i,j,2) = y1(i,j,N); %34
-                
-                x(1,j,k) = x(2,j,k)*exp(-1i*y1(1,j,k)); %35
-                x(N,j,k) = x(N-1,j,k)*exp(1i*y1(N-1,j,k)); %35
-                x(i,1,k) = x(2,j,k)*exp(-1i*y2(i,1,k));
-                x(i,N,k) = x(i,N-1,k)*exp(1i*y2(i,N-1,k));
-            end
-        end
-    end
-    
-    for k = 1:N
-        for j = 1:N
-            for i = 1:N
+    % phi_z in first layer yz
+    y3(1, :, :) = y3_b;
+    y3(2, :, :) = y3_b;
+
+    % phi_z in last layer yz
+    y3(Nx-1, :, :) = y3_b;
+    y3(Nx, :, :) = y3_b;
+
+%     %%% BOUNDARY CONDITIONS %%%
+%     for k = 1:N
+%         for j = 1:N
+%             for i = 1:N
+%                 x(i,j,1) = x(i,j,N-1); %34
+%                 y1(i,j,1) = y1(i,j,N-1); %34
+%                 x(i,j,2) = x(i,j,N); %34
+%                 y1(i,j,2) = y1(i,j,N); %34
+%                 
+%                 x(1,j,k) = x(2,j,k)*exp(-1i*y1(1,j,k)); %35
+%                 x(N,j,k) = x(N-1,j,k)*exp(1i*y1(N-1,j,k)); %35
+%                 x(i,1,k) = x(2,j,k)*exp(-1i*y2(i,1,k));
+%                 x(i,N,k) = x(i,N-1,k)*exp(1i*y2(i,N-1,k));
+%             end
+%         end
+%     end
+%     
+    for k = 1 : Nz-1
+        for j = 1 : Ny-1
+            for i = 2 : Nx-1
                 %%%%%%%% CENTER %%%%%%%%%%%
                 dPhidtX(i,j,k) = (kappa^2/hy^2)*(y1(i,j+1,k)-2*y1(i,j,k)+y1(i,j-1,k))...
                                 +(kappa^2/hz^2)*(y1(i,j,k+1)-2*y1(i,j,k)+y1(i,j,k-1))...
--- construct_FPSI.m
+++ construct_FPSI.m
@@ -1,4 +1,4 @@
-function F = construct_FPSI(x)
+function F_PSI = construct_FPSI(x)
     fun = @(a) (1-conj(a).*a).*a;
-    F = fun(x);
+    F_PSI = fun(x);
 end
