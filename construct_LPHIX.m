b86e1461589d7024c7603afbef392a4d3b3ac4aa
Owen Medeiros <omedeiro@mit.edu>
2021-10-10 13:57:20 -0400

    Update construct_LPHIX.m

--- construct_LPHIX.m
+++ construct_LPHIX.m
@@ -3,6 +3,8 @@ LPHI = zeros(Nx*Ny*Nz);
 for k = 1:Nz
     for j = 1:Ny
         for i = 1:Nx
+            mj = Nx*j-Nx*(j-1);
+            mk = Nx*Ny*k-Nx*Ny*(k-1);
             m = Nx*(j-1)+Nx*Ny*(k-1);
             
             % Diagonal
@@ -14,30 +16,41 @@ for k = 1:Nz
                 LPHI(i+m,i+m) = LPHI(i+m,i+m) -(kappa^2/hy^2 + kappa^2/hz^2);
             end
             
-            
-            if j ~= 1 && j ~= Ny && i~=1
-                LPHI(i+m,i+m-1) = LPHI(i+m,i+m-1) + kappa^2/hy^2;
+            % j TERMS
+            if Ny > 1
+                if j == 1 
+                    LPHI(i+m,i+m+mj) = LPHI(i+m,i+mj) + kappa^2/hy^2;
+                end     
+
+                if j == Ny 
+                    LPHI(i+m,i+m-mj) = LPHI(i+m,i+m-mj) + kappa^2/hy^2;
+                end
+                
+                if j > 1 && j < Ny
+                    LPHI(i+m,i+m+mj) = LPHI(i+m,i+m+mj) + kappa^2/hy^2
+                    LPHI(i+m,i+m-mj) = LPHI(i+m,i+m-mj) + kappa^2/hy^2
+                end
+
             end
-            if j ~= 1 && j ~= Ny && i~=Nx
-                LPHI(i+m,i+m+1) = LPHI(i+m,i+m+1) + kappa^2/hy^2;
-            end
-             
-            if j == 1 && i ~= Nx
-                LPHI(i+m,i+m+1) = LPHI(i+m,i+m+1) + kappa^2/hy^2;
-            end     
-             
-            if j == 1 && i ~=1
-                LPHI(i+m,i+m-1) = LPHI(i+m,i+m-1) + kappa^2/hy^2;
-            end
+
             
-            if j == Ny && i~=1
-                LPHI(i+m,i+m-1) = LPHI(i+m,i+m-1) + kappa^2/hy^2;
+            % k TERMS
+            if Nz > 1
+                if k == 1 
+                    LPHI(i+m,i+m+mk) = LPHI(i+m,i+m+mk) + kappa^2/hz^2;
+                end     
+
+                if k == Nz
+                    LPHI(i+m,i+m-mk) = LPHI(i+m,i+m-mk) + kappa^2/hz^2;
+                end
+
+                if k > 1 && k < Nz
+                    LPHI(i+m,i+m+mk) = LPHI(i+m,i+m+mk) + kappa^2/hz^2;
+                    LPHI(i+m,i+m-mk) = LPHI(i+m,i+m-mk) + kappa^2/hz^2;
+                end
             end
-            
-            if j == Ny && i ~= Nx
-                LPHI(i+m,i+m+1) = LPHI(i+m,i+m+1) + kappa^2/hy^2;
-            end   
-            
        end
     end
+end
+
 end
