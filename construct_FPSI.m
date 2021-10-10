33c1a277d9be13c8aeef5743144fa2fae5fbafbc
mcasteMIT <mcaste@mit.edu>
2021-10-10 17:00:28 -0400

    cubetocolumn

--- construct_FPSI.m
+++ construct_FPSI.m
@@ -1,4 +1,4 @@
-function F_PSI = construct_FPSI(x)
+function F_PSI = construct_FPSI(x, Nx, Ny, Nz)
     fun = @(a) (1-conj(a).*a).*a;
-    F_PSI = fun(x);
+    F_PSI = cube2column(fun(  x(2:Nx,2:Ny,2:Nz)  ));
 end
