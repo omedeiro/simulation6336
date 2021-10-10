78e0b43344106aa4372843d350d6e6ebff8ed87d
Owen Medeiros <omedeiro@mit.edu>
2021-10-10 15:55:09 -0400

    Create cube2column.m

---
+++ cube2column.m
@@ -0,0 +1,10 @@
+function ucol = cube2column(u)
+ucol = zeros(Nx*Ny*Nz,1);
+for k = 1:Nz
+    for j = 1:Ny
+        for i = 1:Nx
+            m = Nx*(j-1)+Nx*Ny*(k-1);
+            ucol(i+m) = u(i,j,k);
+        end
+    end
+end
