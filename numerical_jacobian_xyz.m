db71f7dd574aa61f3a97c3b59302b71624da8fbb
Owen Medeiros <omedeiro@mit.edu>
2021-10-24 19:30:21 -0400

    Update numerical_jacobian_xyz.m

--- numerical_jacobian_xyz.m
+++ numerical_jacobian_xyz.m
@@ -6,11 +6,12 @@ x = sqrt(x/numel(x));
 y1 = zeros(Nx-1, Ny-1, Nz-1);
 y2 = zeros(Nx-1, Ny-1, Nz-1);
 y3 = zeros(Nx-1, Ny-1, Nz-1);
+
 hx = 1;
 hy = 1;
 hz = 1;
 kappa=1;
-Bx=1;
+Bx=1e-3;
 
 U0=Bx;
 X0 = [cube2column(x);cube2column(y1);cube2column(y2);cube2column(y3)];
@@ -21,14 +22,17 @@ FU = @(U) analytical_f_xyz(X0, U, hx, hy, hz, kappa, Nx, Ny, Nz);
 
 % JnumericU = eval_num_jacU(U0, FU);
 
-tstart = tic;
 Jnumeric = eval_num_jac(X0, F, 1e-6);
 JnumericU = eval_num_jac(U0, FU, 1e-6);
 
 
+figure(1)
+subplot(1,2,1)
 imagesc(abs(Jnumeric))
-
-
+axis square
+subplot(1,2,2)
+imagesc(abs(JnumericU))
+axis square
 
 % Janalytic = eval_analytical_jac_xyz(x, y1, y2, y3, hx, hy, hz, kappa, Nx-1);
 % 
