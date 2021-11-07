2fb45cb929bd4eda2e0b0c4ee99f9d53d8cdb5d7
mcasteMIT <mcaste@mit.edu>
2021-11-06 20:16:13 -0400

    jac with sparse

--- eval_num_jac.m
+++ eval_num_jac.m
@@ -1,16 +1,18 @@
-function J = eval_num_jac(X0, F, err)
+function J = eval_num_jac(X0, eval_f, p, eval_u, t, err)
     eps_Im = .01;
     eps_Re = .01;
     S_Im = 2;
     S_Re = 2;
-    J = zeros(size(F(X0),1), numel(X0));
-    Jp = ones(size(F(X0),1), numel(X0));
+    u = feval(eval_u,t);
+   
+    J = sparse(size(feval(eval_f, X0, p, u),1), numel(X0));
+    Jp = sparse(size(feval(eval_f, X0, p, u),1), numel(X0),1);
     while any(abs((J - Jp)) > err, 'all') 
         Jp = J;
         for k = 1 : size(J,2) % loop columns
-            dx = zeros(size(X0,1), 1);
+            dx = sparse(size(X0,1),1);
             dx(k) = eps_Re + 1i*eps_Im;
-            J(:, k) = (F(X0 + dx) - F(X0))/dx(k);
+            J(:, k) = (feval(eval_f, X0+dx, p, u) - feval(eval_f, X0, p, u))/dx(k);
         end
         eps_Re = eps_Re/S_Re;
         eps_Im = eps_Im/S_Im;
