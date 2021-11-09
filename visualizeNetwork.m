17187ca8cb916acfdf4100be30d84d4c9a85963d
Owen Medeiros <omedeiro@mit.edu>
2021-11-09 18:42:40 -0500

    Update visualizeNetwork.m

--- visualizeNetwork.m
+++ visualizeNetwork.m
@@ -2,7 +2,7 @@ function visualizeNetwork(t,X,p)
 
 [Bx,By,Bz] = eval_Bfield(X,p);
 
-[xx, yy, zz] = meshgrid(0:p.hx:p.hx*(p.Nx-2), 0:p.hy:p.hy*(p.Ny-2), 0:p.hz:p.hz*(p.Nz-2));
+[xx, yy, zz] = meshgrid(1:p.hx:p.hx*(p.Nx-1), 1:p.hy:p.hy*(p.Ny-1), 1:p.hz:p.hz*(p.Nz-1));
 
 [xx2, yy2, zz2] = meshgrid(p.hx:p.hx:p.hx*(p.Nx-2), p.hy:p.hy:p.hy*(p.Ny-2), p.hz:p.hz:p.hz*(p.Nz-2));
 xx = cube2column(xx);
@@ -65,54 +65,61 @@ n2 = (p.Nx-2)*(p.Ny-2)*(p.Nz-2);
     sgtitle(t)
     drawnow
     
+    
+    %%
+
     Bavg_x = mean(Bx(:,t),'all', 'omitnan');
     Bavg_y = mean(By(:,t),'all', 'omitnan');
     Bavg_z = mean(Bz(:,t),'all', 'omitnan');
     
     figure(3)
-    q = quiver3(1,1,1,Bavg_x,Bavg_y,Bavg_z)
+    q = quiver3(1,1,1,Bavg_x,Bavg_y,Bavg_z);
+    xlim([0 2])
+    ylim([0 2])
     
-%     figure(3)
-%     PSIT = X(1:n,t);
-%     PHITX = X(n+1:2*n,t);
-%     PHITY = X(2*n+1:3*n,t);
-%     PHITZ = X(3*n+1:4*n,t);
-% 
-%     subplot(2,2,1)
-%     scatter3(xx,yy,zz,36,imag(PSIT), 'filled', 'MarkerFaceAlpha', 0.5)
-%     colorbar
-%     axis equal
-%     xlabel('x')
-%     ylabel('y')
-%     zlabel('z')
-%     
-%     subplot(2,2,2)
-%     scatter3(xx,yy,zz,36,imag(PHITX), 'filled', 'MarkerFaceAlpha', 0.5)
-%     colorbar
-%     axis equal
-%     xlabel('x')
-%     ylabel('y')
-%     zlabel('z')
-%     
-%     subplot(2,2,3)
-%     scatter3(xx,yy,zz,36,real(PHITY), 'filled', 'MarkerFaceAlpha', 0.5)
-%     colorbar
-%     axis equal
-%     xlabel('x')
-%     ylabel('y')
-%     zlabel('z')
-%     
-%     subplot(2,2,4)
-%     scatter3(xx,yy,zz,36,real(PHITZ), 'filled', 'MarkerFaceAlpha', 0.5)
-%     colorbar
-%     axis equal
-%     xlabel('x')
-%     ylabel('y')
-%     zlabel('z')
-%     
-%     pause(0.05)    
-%     sgtitle(t)
-%     drawnow
+    
+    %%
+    figure(4)
+    PSIT = X(1:n,t);
+    PHITX = X(n+1:2*n,t);
+    PHITY = X(2*n+1:3*n,t);
+    PHITZ = X(3*n+1:4*n,t);
+
+    subplot(2,2,1)
+    scatter3(xx,yy,zz,36,abs(PSIT).^2, 'filled', 'MarkerFaceAlpha', 0.5)
+    colorbar
+    axis equal
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    
+    subplot(2,2,2)
+    scatter3(xx,yy,zz,36,abs(PHITX).^2, 'filled', 'MarkerFaceAlpha', 0.5)
+    colorbar
+    axis equal
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    
+    subplot(2,2,3)
+    scatter3(xx,yy,zz,36,abs(PHITY).^2, 'filled', 'MarkerFaceAlpha', 0.5)
+    colorbar
+    axis equal
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    
+    subplot(2,2,4)
+    scatter3(xx,yy,zz,36,abs(PHITZ).^2, 'filled', 'MarkerFaceAlpha', 0.5)
+    colorbar
+    axis equal
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    
+    pause(0.05)    
+    sgtitle(t)
+    drawnow
 %     
 %     figure(4)
 %     subplot(1,3,1)
