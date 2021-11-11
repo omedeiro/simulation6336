9854f71e9622cd70efa1b53b13621f1aa56529ef
Owen Medeiros <omedeiro@mit.edu>
2021-11-10 21:25:01 -0500

    removed old comments and white space

--- construct_FPHIXm.m
+++ construct_FPHIXm.m
@@ -5,10 +5,10 @@ mk = (p.Nx+1)*(p.Ny+1);
 mj = (p.Nx+1);
 
 m = p.M2;
-M = p.m2;              
+M = p.m2;       
 
 FPHIX = sparse(M, 1, (p.kappa^2/p.hy^2).*(-y2(m+1)+y2(m)+y2(m+1-mj)-y2(m-mj))...
-                +(p.kappa^2/p.hz^2).*(-y3(m+1)+y3(m)+y3(m+1-mk)-y3(m-mk))...
-                +imag(exp(-1i*y1(m)).*conj(x(m)).*x(m+1)) );
+                    +(p.kappa^2/p.hz^2).*(-y3(m+1)+y3(m)+y3(m+1-mk)-y3(m-mk))...
+                    +imag(exp(-1i*y1(m)).*conj(x(m)).*x(m+1)) );
        
 end                            
--- construct_FPHIYm.m
+++ construct_FPHIYm.m
@@ -1,21 +1,14 @@
 function FPHIY = construct_FPHIYm(x, y1, y2, y3, p)
-    hx = p.hx;
-%     hy = p.hy;
-    hz = p.hz; 
-    kappa = p.kappa; 
-%     Nx = p.Nx;
-%     Ny = p.Ny;
-%     Nz = p.Nz;
+
     
-    mk = (p.Nx+1)*(p.Ny+1);
-    mj = (p.Nx+1);
-    m = p.M2;
-    M = p.m2;  
+mk = (p.Nx+1)*(p.Ny+1);
+mj = (p.Nx+1);
 
-%     FPHIY = sparse((p.Nx-1)*(p.Ny-1)*(p.Nz-1),1);
+m = p.M2;
+M = p.m2;  
 
-    FPHIY = sparse(M, 1, (kappa^2/hz^2).*(-y3(m+mj)+y3(m)+y3(m+mj-mk)-y3(m-mk))...
-                    +(kappa^2/hx^2).*(-y1(m+mj)+y1(m)+y1(m-1+mj)-y1(m-1))...
+FPHIY = sparse(M, 1, (p.kappa^2/p.hz^2).*(-y3(m+mj)+y3(m)+y3(m+mj-mk)-y3(m-mk))...
+                    +(p.kappa^2/p.hx^2).*(-y1(m+mj)+y1(m)+y1(m-1+mj)-y1(m-1))...
                     +imag(exp(-1i*y2(m)).*conj(x(m)).*x(m+mj)) );
 
         
