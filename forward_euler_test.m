0ea603efdb434b0d12bd017148e30bfd804cf982
Owen Medeiros <omedeiro@mit.edu>
2021-11-16 22:41:16 -0500

    Update forward_euler_test.m
    OM trash testing.

--- forward_euler_test.m
+++ forward_euler_test.m
@@ -6,12 +6,16 @@ eval_u = "analytical_u_xyz";
 eval_f = "eval_f";
 
 p.kappa = 5;
-p.Nx = 10;
-p.Ny = 10;
+p.Nx = 50;
+p.Ny = 50;
 p.Nz = 10;
 p.hx = 1;
 p.hy = 1;
 p.hz = 1;
+
+p.periodic_x = 1;
+p.periodic_y = 1;
+p.periodic_z = 0;
 
 p = contruct_indices(p);
 
@@ -19,35 +23,29 @@ p.LPHIX = construct_LPHIXm(p);
 p.LPHIY = construct_LPHIYm(p);
 p.LPHIZ = construct_LPHIZm(p);
 
-% x = sparse(1:(p.Nx-1)*(p.Ny-1)*(p.Nz-1),1, sqrt(1/((p.Nx-1)*(p.Ny-1)*(p.Nz-1))));
 x = sparse(1:(p.Nx-1)*(p.Ny-1)*(p.Nz-1),1, 1);
 y1 = sparse((p.Nx-1)*(p.Ny-1)*(p.Nz-1),1);
 y2 = sparse((p.Nx-1)*(p.Ny-1)*(p.Nz-1),1);
 y3 = sparse((p.Nx-1)*(p.Ny-1)*(p.Nz-1),1);
 
 x_start = [x;y1;y2;y3];
-% 
-% t = 0.1;
-% 
-% err_J = 1e-2;
-% J = eval_num_jac(x_start, eval_f, p, eval_u, t, err_J)
 
 t_start=0;
-t_stop=.005;
+t_stop=.01;
 max_dt_FE = .0005;
-visualize = 0;
+visualize = 1;
 
 [X] = ForwardEuler(eval_f,x_start,p,eval_u,t_start,t_stop,max_dt_FE,visualize);
 
-%%
-visualizeNetworkFace(X,p)
 
 %% 
+OM=0;
+if OM == 1
 [Bx,By,Bz] = eval_Bfield(X,p);
 
-[xx, yy, zz] = meshgrid(1:p.hx:p.hx*(p.Nx-1), 1:p.hy:p.hy*(p.Ny-1), 1:p.hz:p.hz*(p.Nz-1));
+[yy, xx, zz] = meshgrid(1:p.hx:p.hx*(p.Nx-1), 1:p.hy:p.hy*(p.Ny-1), 1:p.hz:p.hz*(p.Nz-1));
 
-[xx2, yy2, zz2] = meshgrid(p.hx:p.hx:p.hx*(p.Nx-2), p.hy:p.hy:p.hy*(p.Ny-2), p.hz:p.hz:p.hz*(p.Nz-2));
+[yy2, xx2, zz2] = meshgrid(p.hx:p.hx:p.hx*(p.Nx-2), p.hy:p.hy:p.hy*(p.Ny-2), p.hz:p.hz:p.hz*(p.Nz-2));
 xx = cube2column(xx);
 yy = cube2column(yy);
 zz = cube2column(zz);
@@ -59,7 +57,7 @@ zz2 = cube2column(zz2);
 n = (p.Nx-1)*(p.Ny-1)*(p.Nz-1);
 n2 = (p.Nx-2)*(p.Ny-2)*(p.Nz-2);
 figure
-for i = 1:length(p.m)
+for i = 1:p.Nx*p.Ny+p.Nz
     scatter3(xx2(i),yy2(i),zz2(i),36,abs(Bx(i,1)), 'filled', 'MarkerFaceAlpha', 0.5)
     colorbar
 %     caxis([0 max(max(Bx))]);
@@ -71,4 +69,6 @@ for i = 1:length(p.m)
     drawnow
     hold on
     pause(0.05)
+end
+
 end
