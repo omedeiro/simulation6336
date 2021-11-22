c871de812ba9ab354e5b89fc40141a7d37ff691a
Owen Medeiros <omedeiro@mit.edu>
2021-11-22 14:26:29 -0500

    Update visualizeNetwork.m

--- visualizeNetwork.m
+++ visualizeNetwork.m
@@ -79,16 +79,7 @@ if omm ==1
             gif
         end
     end
-    % Capture the plot as an image 
-%     frame = getframe(gcf); 
-%     im = frame2im(frame); 
-%     [imind,cm] = rgb2ind(im,256); 
-%     % Write to the GIF File 
-%     if t == 1 
-%       imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
-%     else 
-%       imwrite(imind,cm,filename,'gif','WriteMode','append'); 
-%     end 
+
 end
     %%
 
