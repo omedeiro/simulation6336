227574540d723046f05b9088a3fa456031136225
Owen Medeiros <omedeiro@mit.edu>
2021-11-23 09:42:54 -0500

    Update eval_u.m
    eval_u not supports x,y,z fields

--- eval_u.m
+++ eval_u.m
@@ -7,36 +7,66 @@ Bx = sparse((p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
 By = sparse((p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
 Bz = sparse((p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
 
-% classicBx = [p.m1y_int p.m1z_int p.mNyp1_int, p.mNzp1_int];
-% classicBy = [p.m1z_int p.m1x_int p.mNzp1_int, p.mNxp1_int];
+classicBx = [p.m1y_int p.m1z_int p.mNyp1_int, p.mNzp1_int];
+classicBy = [p.m1z_int p.m1x_int p.mNzp1_int, p.mNxp1_int];
 classicBz = [p.m1x_int p.m1y_int p.mNxp1_int, p.mNyp1_int];
 
-if t > 0 && t < 50
-    p.appliedBz = 1/(p.hx*p.hy)*p.magBz*t; 
-elseif t > 50 && t < 100
-    p.appliedBz =  1/(p.hx*p.hy)*(p.magBz*(100-t)); 
-% elseif t > 300 && t < 310
-%     p.appliedBz = 1/(p.hx*p.hy)*p.magBz*(t-290); 
-% elseif t > 310 && t < 600
-%     p.appliedBz =  1/(p.hx*p.hy)*p.magBz*20; 
-% elseif t > 600 && t < 610
-%     p.appliedBz =  1/(p.hx*p.hy)*p.magBz*(t-580); 
-% elseif t > 610
-%     p.appliedBz =  1/(p.hx*p.hy)*p.magBz*30; 
-   
+if t > 0 && t <= p.t_stop
+    p.appliedBx = 0; 
+    p.appliedBy = 0; 
+    p.appliedBz = p.magBz; 
+% 
+% elseif t > p.t_stop/4 && t <= p.t_stop*2
+%     p.appliedBx = 0; 
+%     p.appliedBy = 0; 
+%     p.appliedBz = 1/(p.hx*p.hy)*p.magBz; 
+%     
+% elseif t > p.t_stop/2 && t <= p.t_stop*3/4
+%     p.appliedBx = 0; 
+%     p.appliedBy = 0; 
+%     p.appliedBz = 1/(p.hx*p.hy)*p.magBz*2; 
+    
 else
+    p.appliedBx = 0;
+    p.appliedBy = 0;
     p.appliedBz = 0;
+
+end
+
+
+
+%% normalization
+
+%%%%%%% x
+if p.appliedBx>0
+    sumBx = (2*(p.Nx-2)*(p.Ny-2) + 2*(p.Nx-2)*(p.Nz-2))*p.appliedBx - sum(Bx0);
+    sumBx0 = (2*(p.Nx-2)*(p.Ny-2) + 2*(p.Nx-2)*(p.Nz-2))*p.appliedBx;
+    p.Brealx = p.appliedBx*abs(sumBx)/abs(sumBx0);
+    Bx = Bx + sparse(classicBx,1,p.Brealx,(p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
+else
+    p.Brealx = 0;
+    Bx = sparse((p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
+end
+
+
+%%%%%%% y
+if p.appliedBy>0
+    sumBy = (2*(p.Ny-2)*(p.Nx-2) + 2*(p.Ny-2)*(p.Nz-2))*p.appliedBy - sum(By0);
+    sumBy0 = (2*(p.Ny-2)*(p.Nx-2) + 2*(p.Ny-2)*(p.Nz-2))*p.appliedBy;
+    p.Brealy = p.appliedBy*abs(sumBy)/abs(sumBy0);
+    By = By + sparse(classicBy,1,p.Brealy,(p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
+else
+    p.Brealy = 0;
+    By = sparse((p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
 end
 
 if p.appliedBz>0
-    sumBz = (2*(p.Nz+1)*(p.Nx+1) + 2*(p.Nz+1)*(p.Ny+1))*p.appliedBz - sum(Bz0);
-    sumBz0 = (2*(p.Nz+1)*(p.Nx+1) + 2*(p.Nz+1)*(p.Ny+1))*p.appliedBz;
-    p.Breal = p.appliedBz*abs(sumBz)/abs(sumBz0);
-    Bz = Bz + sparse(classicBz,1,p.Breal,(p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
+    sumBz = (2*(p.Nz-2)*(p.Nx-2) + 2*(p.Nz-2)*(p.Ny-2))*p.appliedBz - sum(Bz0);
+    sumBz0 = (2*(p.Nz-2)*(p.Nx-2) + 2*(p.Nz-2)*(p.Ny-2))*p.appliedBz;
+    p.Brealz = p.appliedBz*abs(sumBz)/abs(sumBz0);
+    Bz = Bz + sparse(classicBz,1,p.Brealz,(p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
 else
-    p.Breal = 0;
-    Bx = sparse((p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
-    By = sparse((p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
+    p.Brealz = 0;
     Bz = sparse((p.Nx+1)*(p.Ny+1)*(p.Nz+1), 1);
 end
 
