5446322508c63c8fd2576295580392e1104129c5
Owen Medeiros <omedeiro@mit.edu>
2021-11-25 14:55:24 -0500

    2D

--- construct_FPHIYm.m
+++ construct_FPHIYm.m
@@ -6,10 +6,13 @@ mj = (p.Nx+1);
 
 m = p.M2;
 M = p.m2;  
-
-FPHIY = sparse(M, 1, (p.kappa^2/p.hz^2).*(-y3(m+mj)+y3(m)+y3(m+mj-mk)-y3(m-mk))...
-                    +(p.kappa^2/p.hx^2).*(-y1(m+mj)+y1(m)+y1(m+mj-1)-y1(m-1))...
-                    +imag(exp(-1i*y2(m)).*conj(x(m)).*x(m+mj)) );
-
+if p.Nz>1
+    FPHIY = sparse(M, 1, (p.kappa^2/p.hz^2).*(-y3(m+mj)+y3(m)+y3(m+mj-mk)-y3(m-mk))...
+                        +(p.kappa^2/p.hx^2).*(-y1(m+mj)+y1(m)+y1(m+mj-1)-y1(m-1))...
+                        +imag(exp(-1i*y2(m)).*conj(x(m)).*x(m+mj)) );
+else
+    FPHIY = sparse(M, 1, (p.kappa^2/p.hx^2).*(-y1(m+mj)+y1(m)+y1(m+mj-1)-y1(m-1))...
+                        +imag(exp(-1i*y2(m)).*conj(x(m)).*x(m+mj)) );
+end
 
 end
