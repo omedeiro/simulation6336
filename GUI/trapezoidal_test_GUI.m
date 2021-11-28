74e1819a3b5bd5b7d709c985064e6dc6d89bfee8
mcasteMIT <mcaste@mit.edu>
2021-11-28 13:29:55 -0500

    GUI

---
+++ GUI/trapezoidal_test_GUI.m
@@ -0,0 +1,94 @@
+function trapezoidal_test_GUI(app)
+
+% clear all
+% close all
+
+% eval_f = "eval_f_B";
+% eval_u = "analytical_u_xyz";
+
+eval_f = "eval_f_GUI";
+eval_u = "eval_u_GUI";
+
+% global p
+% global cord
+% global click_location
+% 
+% click_location = [];
+
+p.kappa = 5;
+p.Nx = 30;
+p.Ny = 30;
+p.Nz = 3;
+p.hx = 1;
+p.hy = 1;
+p.hz = 1;
+
+p.magBx = 0;
+p.magBy = 0;
+p.magBz = 50;
+p.appliedBz = 0;
+p.periodic_x = 0;
+p.periodic_y = 0;
+p.periodic_z = 0;
+p.slice=1;
+p = contruct_indices(p);
+
+% 
+% cord = [1 (p.Ny-1)];
+% 
+
+p.LPHIX = construct_LPHIXm(p);
+p.LPHIY = construct_LPHIYm(p);
+p.LPHIZ = construct_LPHIZm(p);
+
+if p.Nz > 1
+    x = sparse(1:(p.Nx-1)*(p.Ny-1)*(p.Nz-1),1, 1);
+    y1 = sparse((p.Nx-1)*(p.Ny-1)*(p.Nz-1),1);
+    y2 = sparse((p.Nx-1)*(p.Ny-1)*(p.Nz-1),1);
+    y3 = sparse((p.Nx-1)*(p.Ny-1)*(p.Nz-1),1);
+else
+    x = sparse(1:(p.Nx-1)*(p.Ny-1),1, 1);
+    y1 = sparse((p.Nx-1)*(p.Ny-1),1);
+    y2 = sparse((p.Nx-1)*(p.Ny-1),1);
+    y3 = [];
+end
+
+% initialize as quenched normal state
+
+% for i = 1:(p.Nx-1)*(p.Ny-1)*(p.Nz-1)
+%     random = rand(1, 1);
+%     rphaser = rand(1, 1);
+%     rphasei = rand(1, 1);
+%     if random < 0.5
+%         x(i, 1) = 1 - random*(rphaser + 1i*rphasei)/(sqrt(rphaser^2 + rphasei^2));
+%     end
+% end
+
+% initialize w/vortex
+%{
+offset = 7;
+for i=1:8
+    for j = 1:8
+        x(index_map(offset+i, offset+j, 1, p)) = 0;
+    end
+end
+%}
+
+
+%%
+x_start = [x;y1;y2;y3];
+p.linearize = 0;
+p.cord = 0;
+p.t_start=0;
+p.t_stop=100;
+p.timestep = 1e-1;
+visualize = 1;
+p.visualizeSave = 0;
+[X,p] = Trapezoidal_GUI(eval_f,x_start,p,eval_u,p.t_start,p.t_stop,p.timestep,visualize, app);
+
+
+%% 
+p.frames = 40;
+p.visualizeSave=0;
+% visualizeNetworkX2d(X,p)
+end
---
+++ GUI/visualizeNetworkX2d_GUI.m
@@ -0,0 +1,178 @@
+function p = visualizeNetworkX2dInteract(t,X,p, app)
+
+
+%     [Bx,By,Bz] = eval_Bfield(X,p);
+Bx = p.BXT;
+By = p.BYT;
+Bz = p.BZT;
+
+[yy, xx] = meshgrid(p.hx:p.hx:p.hx*(p.Nx-1), p.hy:p.hy:p.hy*(p.Ny-1));
+
+[yy2, xx2] = meshgrid(p.hx:p.hx:p.hx*(p.Nx-2), p.hy:p.hy:p.hy*(p.Ny-2));
+xx = cube2column(xx);
+yy = cube2column(yy);
+xx2 = cube2column(xx2);
+yy2 = cube2column(yy2);
+mk = (p.Nx-1)*(p.Ny-1);
+mk2 = (p.Nx-2)*(p.Ny-2);
+
+n = (p.Nx-1)*(p.Ny-1)*(p.Nz-1);
+n2 = (p.Nx-2)*(p.Ny-2)*(p.Nz-2);
+
+
+% Slice
+if p.slice > 0
+    ss = mk * (p.slice - 1) + 1;
+    se   = mk * (p.slice);
+    ss2 = mk2 * (p.slice - 1) + 1;
+    se2   = mk2 * (p.slice);
+else
+    ss = 1;
+    se = n;
+    ss2 = 1;
+    se2 = n;
+end
+
+
+
+PSIT = X(1:n,t);
+PHITX = X(n+1:2*n,t);
+PHITY = X(2*n+1:3*n,t);
+PHITZ = X(3*n+1:4*n,t);
+
+PSIT = PSIT(ss:se);
+PHITX = PHITX(ss:se);
+PHITY = PHITY(ss:se);
+PHITZ = PHITZ(ss:se);
+xx = cube2column(xx);
+yy = cube2column(yy);
+PSIT = reshape(PSIT,p.Nx-1,p.Ny-1);
+PHITX = reshape(PHITX,p.Nx-1,p.Ny-1);
+PHITY = reshape(PHITY,p.Nx-1,p.Ny-1);
+PHITZ = reshape(PHITZ,p.Nx-1,p.Ny-1);
+
+h1 = app.UIAxes;
+% h1.Position = [900 200 550 800];
+
+imagesc(app.UIAxes,abs(PSIT).^2./max(abs(PSIT).^2,1))
+% ax = gca;
+set(app.UIAxes,'YDir','normal')
+colorbar(app.UIAxes)
+caxis(app.UIAxes,[0 1])
+axis(app.UIAxes, 'equal')
+axis(app.UIAxes, 'tight')
+axis(app.UIAxes, 'off')
+title(app.UIAxes,'|\psi|^2')
+% xlabel(app.UIAxes,'x')
+% ylabel(app.UIAxes,'y')
+% zlabel(app.UIAxes,'z')
+
+% 
+% subplot(5,3,10)
+% imagesc(xx,yy,real(PHITX))
+% colorbar
+% title('\phi_x')
+% axis equal
+% axis tight
+% axis off
+% xlabel('x')
+% ylabel('y')
+% zlabel('z')
+% 
+% subplot(5,3,11)
+% imagesc(xx,yy,real(PHITY))
+% colorbar
+% title('\phi_y')
+% axis equal
+% axis tight
+% axis off
+% xlabel('x')
+% ylabel('y')
+% zlabel('z')
+% 
+% subplot(5,3,12)
+% imagesc(xx,yy,real(PHITZ))
+% colorbar
+% title('\phi_z')
+% axis equal
+% axis tight
+% axis off
+% xlabel('x')
+% ylabel('y')
+% zlabel('z')
+% 
+% 
+% 
+% 
+% 
+% 
+Bxx  = reshape(Bx(ss2:se2,t-1),p.Nx-2,p.Ny-2);
+Byy  = reshape(By(ss2:se2,t-1),p.Nx-2,p.Ny-2);
+Bzz  = reshape(Bz(ss2:se2,t-1),p.Nx-2,p.Ny-2);
+% 
+% subplot(5,3,13)
+% imagesc(xx2,yy2,real(Bxx))
+% colorbar
+% axis equal
+% axis tight
+% axis off
+% title('B_x')
+% xlabel('x')
+% ylabel('y')
+% zlabel('z')
+% 
+% 
+% subplot(5,3,14)
+% imagesc(xx2,yy2,real(Byy))
+% colorbar
+% axis equal
+% axis tight
+% axis off
+% title('B_y')
+% xlabel('x')
+% ylabel('y')
+% zlabel('z')
+% 
+% figure(2)
+imagesc(app.UIAxes2,real(Bzz))
+set(app.UIAxes2,'YDir','normal')
+colorbar(app.UIAxes2)
+caxis(app.UIAxes2, 'auto')
+axis(app.UIAxes2, 'equal')
+axis(app.UIAxes2, 'tight')
+axis(app.UIAxes2, 'off')
+title(app.UIAxes2, 'B_z')
+% xlabel(app.UIAxes2, 'x')
+% ylabel(app.UIAxes2, 'y')
+% zlabel(app.UIAxes2, 'z')
+
+% sgtitle({"applied Bx = "+p.Brealxt(t-1)+", applied By = "+p.Brealyt(t-1)+", applied Bz = "+p.Brealzt(t-1),"t = "+ p.t(t),})
+
+drawnow;
+if p.visualizeSave == 1
+    if t ==1
+        addpath(pwd+"/gifs")
+        filename = char(pwd+"/gifs/trapGif"+datestr(now,30)+".gif");
+        gif(filename);
+        save(filename(1:end-4),'p')
+    else
+        gif
+    end
+end
+
+h1.ButtonDownFcn = @mouseClick;
+function mouseClick(~,~)
+    global cord
+    global click
+    c = get(app.UIAxes,'CurrentPoint');
+    cord = c;
+    click = 1;
+end
+
+
+
+%%
+
+end
+
+
